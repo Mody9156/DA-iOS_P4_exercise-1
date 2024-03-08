ca7a9181eb47f6032edb92ef6a09ea8150d15e89
Modibo KEITA <modykeita2@gmail.com>
2024-03-08 15:49:29 +0100

    update de la view ToDoListViewModel.swift

--- ToDoList/Sources/ViewModel/ToDoListViewModel.swift
+++ ToDoList/Sources/ViewModel/ToDoListViewModel.swift
@@ -2,46 +2,61 @@ import SwiftUI
 
 final class ToDoListViewModel: ObservableObject {
     // MARK: - Private properties
-
+    
     private let repository: ToDoListRepositoryType
-
+    private var arrayOfOTodLit : [ToDoItem] = []
+    private var curentFilterIndex : Int = 0
+    private var filteredTodoItems : [ToDoItem] = []
     // MARK: - Init
-
+    
     init(repository: ToDoListRepositoryType) {
         self.repository = repository
         self.toDoItems = repository.loadToDoItems()
+        self.toDoItems = self.arrayOfOTodLit
+        
     }
-
+    
     // MARK: - Outputs
-
+    
     /// Publisher for the list of to-do items.
     @Published var toDoItems: [ToDoItem] = [] {
         didSet {
             repository.saveToDoItems(toDoItems)
         }
     }
-
+    
     // MARK: - Inputs
-
+    
     // Add a new to-do item with priority and category
     func add(item: ToDoItem) {
-        toDoItems.append(item)
+        arrayOfOTodLit.append(item)
+        applyFilter(at:curentFilterIndex)
+        
     }
-
+    
     /// Toggles the completion status of a to-do item.
     func toggleTodoItemCompletion(_ item: ToDoItem) {
         if let index = toDoItems.firstIndex(where: { $0.id == item.id }) {
-            toDoItems[index].isDone.toggle()
+            arrayOfOTodLit[index].isDone.toggle()
+            applyFilter(at:curentFilterIndex)
         }
     }
-
+    
     /// Removes a to-do item from the list.
     func removeTodoItem(_ item: ToDoItem) {
-        toDoItems.removeAll { $0.id == item.id }
+        arrayOfOTodLit.removeAll { $0.id == item.id }
+        applyFilter(at:curentFilterIndex)
     }
-
+    
     /// Apply the filter to update the list.
     func applyFilter(at index: Int) {
         // TODO: - Implement the logic for filtering
+        switch index {
+        case 1 : toDoItems = arrayOfOTodLit.filter{$0.isDone}
+        case 2 : toDoItems = arrayOfOTodLit.filter{!$0.isDone}
+        default: toDoItems = arrayOfOTodLit
+        
+        }
+        curentFilterIndex = index
     }
 }
